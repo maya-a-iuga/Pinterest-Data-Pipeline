dce1c2d1567f794cfaf6c9a52f7fac5a66c31e3d
Maya Iuga <maya_iuga@yahoo.ro>
2022-06-10 11:02:40 +0100

    Adds Kafka producer

--- API/project_pin_API.py
+++ API/project_pin_API.py
@@ -1,11 +1,11 @@
+from ensurepip import bootstrap
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 from json import dumps
-#from kafka import KafkaProducer
+from kafka import KafkaProducer
 
 app = FastAPI()
-
 
 
 class Data(BaseModel):
@@ -21,11 +21,20 @@ class Data(BaseModel):
     downloaded: int
     save_location: str
 
+#Configure producer which will send data to the KafkaPinterestTopic
+#value_serializer = converts message sent to topic into bytes
+pin_producer = KafkaProducer(
+    bootstrap_servers = "localhost:9092",
+    client_id = "Pinterest_producer",
+    value_serializer = lambda pinmessage: dumps(pinmessage).encode("ascii")
+)
+
 
 @app.post("/pin/")
 def get_db_row(item: Data):
     data = dict(item)
-    return item
+    pin_producer.send(topic = "KafkaPinterestTopic", value = data)
+    #return item
 
 
 if __name__ == '__main__':
