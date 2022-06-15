af90e9b3a3f1bb9926760f64ea903e1151a0b673
Maya Iuga <maya_iuga@yahoo.ro>
2022-06-15 10:01:25 +0100

    Updates Kafka Producer and Consumers

---
+++ Kafka/Batch_Kafka_Consumer.py
@@ -0,0 +1,30 @@
+from kafka import KafkaConsumer
+from json import loads
+from json import dumps
+import boto3
+
+# Create Consumer to retrieve the messages from the topic
+batch_pin_consumer = KafkaConsumer(
+    bootstrap_servers = "localhost:9092",
+    value_deserializer = lambda pinmessage: loads(pinmessage),
+    #ensures messages are read from the beggining
+    auto_offset_reset = "earliest"
+)
+
+batch_pin_consumer.subscribe(topics = "KafkaPinterestTopic")
+#for message in batch_pin_consumer:
+#   print(message)
+
+# connect to desired S3 bucket
+s3_resource = boto3.resource('s3')
+bucket_name = "pinterestpipelinebucket"
+my_bucket = s3_resource.Bucket(bucket_name)
+
+# send data from Consumer to S3 bucket
+counter = 0
+for message in batch_pin_consumer:
+    
+    json_object = dumps(message.value)
+    s3_filename = 'events/' + str(counter) + '.json'
+    my_bucket.put_object(Key = s3_filename , Body = json_object)
+    counter += 1
---
+++ Kafka/Streaming_Kafka_Consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+from json import loads
+
+# Create Consumer to retrieve the messages from the topic
+stream_pin_consumer = KafkaConsumer(
+    bootstrap_servers = "localhost:9092",
+    value_deserializer = lambda pinmessage: loads(pinmessage),
+    #ensures messages are read from the beggining
+    auto_offset_reset = "earliest"
+)
+
+stream_pin_consumer.subscribe(topics = "KafkaPinterestTopic")
+#for message in stream_pin_consumer:
+#    print(message)
