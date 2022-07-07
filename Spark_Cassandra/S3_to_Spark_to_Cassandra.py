6121963bc07f49e9dd2e03a12d17a29c4a9823b8
Maya Iuga <maya_iuga@yahoo.ro>
2022-07-07 11:56:23 +0100

    Updates data cleaning methods

--- Spark_Cassandra/S3_to_Spark_to_Cassandra.py
+++ Spark_Cassandra/S3_to_Spark_to_Cassandra.py
@@ -6,48 +6,89 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.functions import when, col, regexp_replace, split
 
-# Creating our Spark configuration
-conf = SparkConf() \
-    .setAppName('S3toSpark') \
-    .setMaster('local[*]') #setting master to run locally
+class Spark_DAG():
 
-sc=SparkContext(conf=conf)
+    def __init__(self):
 
-# Configure the setting to read from the S3 bucket
-accessKeyId = os.environ["AWS_ACCESS_KEY"]
-secretAccessKey = os.environ["AWS_SECRET_ACCESS_KEY"]
-hadoopConf = sc._jsc.hadoopConfiguration()
-hadoopConf.set('fs.s3a.access.key', accessKeyId)
-hadoopConf.set('fs.s3a.secret.key', secretAccessKey)
-hadoopConf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
+        # Creating our Spark configuration
+        conf = SparkConf() \
+            .setAppName('S3toSpark') \
+            .setMaster('local[*]') #setting master to run locally
 
-# Create our Spark session
-spark=SparkSession(sc)
+        sc=SparkContext(conf=conf)
 
-# Read jsons from S3 bucket
-df = spark.read.json("s3a://pinterestpipelinebucket/events/*.json") 
+        # Configure the setting to read from the S3 bucket
+        accessKeyId = os.environ["AWS_ACCESS_KEY"]
+        secretAccessKey = os.environ["AWS_SECRET_ACCESS_KEY"]
+        hadoopConf = sc._jsc.hadoopConfiguration()
+        hadoopConf.set('fs.s3a.access.key', accessKeyId)
+        hadoopConf.set('fs.s3a.secret.key', secretAccessKey)
+        hadoopConf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
 
-# Code to clean out the data
-# transforms K and M into corresponding 0s & changes data type from string to integer
-df = df.withColumn("follower_count", when(col('follower_count').like("%k"), (regexp_replace('follower_count', 'k', '').cast('int')*1000)) \
-    .when(col('follower_count').like("%M"), (regexp_replace('follower_count', 'M', '').cast('int')*1000000))\
-    .when(col('follower_count').like("%B"), (regexp_replace('follower_count', 'B', '').cast('int')*1000000000))) \
-    .fillna({'follower_count' : '0'}) 
-# changes data type from longint to int  
-df= df.withColumn("downloaded", df["downloaded"].cast("int")) 
-df= df.withColumn("index", df["index"].cast("int"))
-# removes 'Save data in' part of the string -> only path left now
-df = df.withColumn('save_location', regexp_replace('save_location', 'Local save in ', ''))
-# renames column for Cassandra table
-df = df.withColumnRenamed("index", "ind")
+        # Create our Spark session
+        self.spark=SparkSession(sc)
 
-# send data from Spark to Cassandra
-df.write \
-  .format("org.apache.spark.sql.cassandra") \
-  .mode("overwrite") \
-  .option("confirm.truncate", "true") \
-  .option("spark.cassandra.connection.host", "127.0.0.1") \
-  .option("spark.cassandra.connection.port", "9042") \
-  .option("keyspace" , "sparkdb") \
-  .option("table", "pin") \
-  .save()
+
+    def read_from_s3(self):
+        # Read jsons from S3 bucket
+        self.df = self.spark.read.json("s3a://pinterestpipelinebucket/events/*.json")
+        
+
+    def transform_spark_data(self):
+
+        # transforms follower_count column into int type
+        self.df = self.df.withColumn("follower_count", when(col('follower_count').like("%k"), regexp_replace('follower_count', 'k', '000')) \
+                            .when(col('follower_count').like("%M"), regexp_replace('follower_count', 'M', '000000'))\
+                            .when(col('follower_count').like("%B"), regexp_replace('follower_count', 'B', '000000000')) \
+                            .cast("int")) 
+
+        # removes 'Save data in' part of the string -> only path left now
+        self.df = self.df.withColumn('save_location', regexp_replace('save_location', 'Local save in ', '')) 
+
+        # changes data type to int  
+        self.df= self.df.withColumn("downloaded", self.df["downloaded"].cast("int")) \
+                            .withColumn("index", self.df["index"].cast("int")) 
+
+        # replace empty values with null for all columns
+        self.df=self.df.select([when(col(c)=="",None).otherwise(col(c)).alias(c) for c in self.df.columns])
+
+        # renames column for Cassandra table
+        self.df = self.df.withColumnRenamed("index", "ind")
+        
+        # reorders columns
+        self.df = self.df.select('ind',
+                                'unique_id',
+                                'title',
+                                'description',
+                                'follower_count',
+                                'tag_list',
+                                'is_image_or_video',
+                                'image_src',
+                                'downloaded',
+                                'save_location',
+                                'category'
+                                )
+
+    def write_to_cassandra(self):
+
+        # send data from Spark to Cassandra
+        self.df.write \
+        .format("org.apache.spark.sql.cassandra") \
+        .mode("overwrite") \
+        .option("confirm.truncate", "true") \
+        .option("spark.cassandra.connection.host", "127.0.0.1") \
+        .option("spark.cassandra.connection.port", "9042") \
+        .option("keyspace" , "sparkdb") \
+        .option("table", "pin") \
+        .save()
+
+
+    def run_spark_session(self):
+        self.read_from_s3()
+        self.transform_spark_data()
+        self.write_to_cassandra()
+
+if __name__ == "__main__":
+
+    spark_session = Spark_DAG()
+    spark_session.run_spark_session()
